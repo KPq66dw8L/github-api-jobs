966e6fdb3f1dbc1b8dc1dac1f2dd056c838b942c
KPq66dw8L <kezhkia@protonmail.com>
2021-04-15 16:28:07 +0200

    elements redirect to blank page

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
+
+
 $('a:first-child').click( function(e) {
   e.preventDefault(); 
   let jobName = encodeURIComponent(document.getElementById('job-name').value);
@@ -13,14 +15,15 @@ $('a:first-child').click( function(e) {
   $.ajax(settings).done(function (response) {
     
     /* Set the grid size depending on the number of elements to load */
-    document.getElementById("results").style.gridTemplateRows = "repeat("+(Math.floor(response.length / 3)+1)+", 1fr)";
+    document.getElementById("results").style.gridTemplateRows = "repeat("+(Math.floor(response.length / 3)+1)+", 300px)";
     /* Fill result's elements */
     $(document).ready(function(){
       for(var i = 0; i< response.length; i++)
-        $("#results").append("<div class='element'><p>Xh ago . "+response[i].type+"</p><h2>"+response[i].title+"</h2><p>"+response[i].company+"</p><h4>Location</h4></div>"); 
+        $("#results").append("<div class='element'><a id='element' href='description.html' style='color:black;'><p>Xh ago . "+response[i].type+"</p><h2>"+response[i].title+"</h2><p>"+response[i].company+"</p><h4>Location</h4></a></div>"); 
         
         console.log(response[0].created_at); 
    });
+   
 
   });
 
