2dfde4cec9d87fee9b32b8dc8008eb9d0d0c5e51
KPq66dw8L <kezhkia@protonmail.com>
2021-04-15 22:00:19 +0200

    description can access element data

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,11 @@
 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 
 $('a:first-child').click( function(e) {
   e.preventDefault(); 
@@ -13,13 +20,20 @@ $('a:first-child').click( function(e) {
   };
   console.log(settings.url);
   $.ajax(settings).done(function (response) {
-    
     /* Set the grid size depending on the number of elements to load */
     document.getElementById("results").style.gridTemplateRows = "repeat("+(Math.floor(response.length / 3)+1)+", 300px)";
     /* Fill result's elements */
     $(document).ready(function(){
       for(var i = 0; i< response.length; i++)
-        $("#results").append("<div class='element' id='"+response[i].id+"'><a  href='description.html?id="+response[i].id+"' style='color:black;'><p>Xh ago . "+response[i].type+"</p><h2>"+response[i].title+"</h2><p>"+response[i].company+"</p><h4>Location</h4></a></div>");    
+        $("#results").append("<div class='element' id='"+i+"'><a href='description.html?id="+response[i].id+"' style='color:black;'><p>Xh ago . "+response[i].type+"</p><h2>"+response[i].title+"</h2><p>"+response[i].company+"</p><h4>Location</h4></a></div>");  
+      /* Event handler for job offer */
+      $(document).ready(function() {
+        $(".element").click(function() {
+            console.log(response[this.id]);
+            /* setCookie('jobOffer', response[this.id], 7); */
+            sessionStorage.setItem('jobOffer', JSON.stringify(response[this.id]));
+        });
+      });
    });
    
   });
--- JS/description.js
+++ JS/description.js
@@ -3,7 +3,7 @@
 /* console.log(window.location.search.slice(4)); */
 
 
-var settings = {
+/* var settings = {
     "url": "https://cors.bridged.cc/https://jobs.github.com/positions.json?id="+window.location.search.slice(4),
     "method": "GET",
     "timeout": 0,
@@ -12,4 +12,24 @@ var settings = {
   $.ajax(settings).done(function (response) {
     console.log(settings.url);
     console.log(response);
-  });
+  }); */
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+let job = $.parseJSON(sessionStorage.getItem('jobOffer'));
+
+console.log(job);
