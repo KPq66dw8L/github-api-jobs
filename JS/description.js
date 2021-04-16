1a35e394cd30c530960c5ec2ed34011dbf87ae8f
KPq66dw8L <kezhkia@protonmail.com>
2021-04-16 12:43:22 +0200

    description page light integrated (no how to apply)

--- JS/description.js
+++ JS/description.js
@@ -1,35 +1,12 @@
-
-
-/* console.log(window.location.search.slice(4)); */
-
-
-/* var settings = {
-    "url": "https://cors.bridged.cc/https://jobs.github.com/positions.json?id="+window.location.search.slice(4),
-    "method": "GET",
-    "timeout": 0,
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(settings.url);
-    console.log(response);
-  }); */
-
-  function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-
 let job = $.parseJSON(sessionStorage.getItem('jobOffer'));
 
 console.log(job);
+
+$("#intro").append("<div><h2>"+job.company+"</h2><p>"+job.company_url+"</p></div> <div class='divBtn'> <a class='btn btn-2' href='"+job.company_url+"' rel='noopener noreferrer' target='_blank'>Company Site</a></div>");
+
+$("#description").append("<div class='subDescription'><div><p>"+job.created_at+" . "+job.type+"</p><h1>"+job.title+"</h1><h4>"+job.location+"</h4></div><div class='divBtn'><a class='btn btn-1' href='"+job.how_to_apply+"' rel='noopener noreferrer' target='_blank'>Apply Now</a></div></div><p>"+job.description+"</p>");
+
+
+$("#description").append("");
+
+$("#footer").append("<div><h3>"+job.title+"</h3><p>"+job.company+"</p></div><div class='divBtn'><a class='btn btn-1' href='#' rel='noopener noreferrer' target='_blank'>Apply Now</a></div>");
